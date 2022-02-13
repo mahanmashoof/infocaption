ee458c6ff7fbd4661c5e693748792ff49450ebe0
Mahan Mashoof <mahan.mashoof@gmail.com>
2022-02-13 10:35:05 -0300

    search field pos ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import Card from "./Card";
+import "./App.css";
 
 function App() {
   const [hitsPerPage, setHitsPerPage] = useState(10);
+  const [freeSearch, setFreeSearch] = useState("");
   const [objArr, setObjArr] = useState([]);
 
   const handleHitsPerPage = (e) => {
-    setHitsPerPage(e.target.value);
+    setHitsPerPage(Number(e.target.value));
   };
 
-  const url = `https://support.infocaption.com/API/lucene/guidesearch?hitsPerPage=${hitsPerPage}`;
+  const handleFreeSearch = (e) => {
+    setFreeSearch(e.target.value);
+  };
+
+  const url = `https://support.infocaption.com/API/lucene/guidesearch?hitsPerPage=${hitsPerPage}&searchQuery=${freeSearch}`;
 
   const handleSearch = () => {
     fetch(url)
@@ -20,17 +26,36 @@ function App() {
       });
   };
 
+  console.log(hitsPerPage);
+
   return (
     <div className="App">
-      <h1>Guider</h1>
-      <form>
-        <input
-          type="number"
-          placeholder="Hits per page"
-          onChange={handleHitsPerPage}
-        />
-      </form>
-      <button onClick={handleSearch}>Search</button>
+      <h1 className="heading">Guider</h1>
+      <div className="search-fields">
+        <form>
+          <input
+            type="text"
+            placeholder="Keywords"
+            onChange={handleFreeSearch}
+          />
+        </form>
+        <form>
+          <label htmlFor="hits-select">Hits per page: </label>
+          <select
+            onChange={handleHitsPerPage}
+            defaultValue="10"
+            id="hits-select"
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="25">25</option>
+            <option value="30">30</option>
+          </select>
+        </form>
+        <button onClick={handleSearch}>Search</button>
+      </div>
       {objArr.length > 0 &&
         objArr.map((obj, i) => (
           <Card
